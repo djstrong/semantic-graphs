cf484c032e9a14cb47b602afb3349ba6a99baad6
djstrong <djstrong@gmail.com>
2014-03-16 13:28:14 +0100

    find connected component and all metrics

---
+++ utils/find_connected_components.py
@@ -0,0 +1,30 @@
+import networkx as nx
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+# Convert graph to undirected, finds the biggest connected components and write it to files.
+
+NUMBER_OF_COMPONENTS = 5
+
+path = sys.argv[1]
+
+G = nx.read_gexf(path)
+G = nx.Graph(G)
+
+print 'number_of_nodes', nx.number_of_nodes(G)
+print 'number_of_edges', nx.number_of_edges(G)
+print 'number_strongly_connected_components', nx.number_strongly_connected_components(G)
+print
+
+strongly_connected_components = nx.strongly_connected_component_subgraphs(G)
+
+name = path.rsplit('.', 1)[0]
+
+for i, component in enumerate(strongly_connected_components[:NUMBER_OF_COMPONENTS]):
+  print i
+  print 'number_of_nodes', nx.number_of_nodes(component)
+  print 'number_of_edges', nx.number_of_edges(component)
+  print
+  
+  nx.write_gexf(component, name+str(i)+'.gexf')
---
+++ utils/metrics.py
@@ -0,0 +1,33 @@
+import networkx as nx
+import collections
+from progress.bar import Bar
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+path = sys.argv[1]
+G = nx.read_gexf(path)
+
+print 'number_of_nodes', nx.number_of_nodes(G)
+print 'number_of_edges', nx.number_of_edges(G)
+print 'number_strongly_connected_components', nx.number_strongly_connected_components(G)
+
+print 'average_clustering', nx.average_clustering(G) # C
+print 'degree_histogram', nx.degree_histogram(G) # P(k)
+  
+diameter = 0
+summ = 0
+count = 0
+
+for n in Bar('Processing', suffix = '%(index)d/%(max)d %(percent).1f%% - %(eta_td)s').iter(G):
+  paths = nx.single_source_shortest_path_length(G,n)
+  for length in paths.itervalues():
+    summ += length
+    count += 1
+    if length > diameter: diameter = length
+
+average_shortest_path_length = float(summ)/count
+print 'diameter', diameter # D
+print 'average_shortest_path_length', average_shortest_path_length # L
+  
+
